9e28d1280a5038dea4b89a378c383908a7b3146f
bruceleroyjr <bruceleroyjr@gmail.com>
2019-03-01 17:42:10 -1000

    Updated Colleague Service Client to include F09 custom code

---
+++ source/Ellucian.Colleague.Api/Ellucian.Colleague.Api.Client/ColleagueApiClient.F09.Custom.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Ellucian.Colleague.Dtos.F09;
+using Ellucian.Web.Utility;
+using Newtonsoft.Json;
+
+namespace Ellucian.Colleague.Api.Client
+{
+    public partial class ColleagueApiClient
+    {
+        public async Task<GetActiveRestrictionsResponseDto> GetF09ActiveRestrictionsAsync(string personId)
+        {
+            if (string.IsNullOrEmpty(personId))
+            {
+                throw new ArgumentNullException("personId", "ID cannot be empty/null for User Profile retrieval.");
+            }
+            try
+            {
+                var baseUrl = UrlUtility.CombineUrlPath(F09ActiveRestrictions, personId);
+
+                var headers = new NameValueCollection();
+                headers.Add(AcceptHeaderKey, _mediaTypeHeaderVersion1);
+                var response = await ExecuteGetRequestWithResponseAsync(baseUrl, headers: headers);
+                var myProfile = JsonConvert.DeserializeObject<GetActiveRestrictionsResponseDto>(await response.Content.ReadAsStringAsync());
+
+                return myProfile;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Unable to GetF09ActiveRestrictionsAsync");
+                throw;
+            }
+        }
+
+        public async Task<UpdateStudentRestrictionResponseDto> GetF09StudentRestrictionAsync(string personId)
+        {
+            if (string.IsNullOrEmpty(personId))
+            {
+                throw new ArgumentNullException("personId", "ID cannot be empty/null for User Profile retrieval.");
+            }
+            try
+            {
+                var baseUrl = UrlUtility.CombineUrlPath(F09StudentRestriction, personId);
+
+                var headers = new NameValueCollection();
+                headers.Add(AcceptHeaderKey, _mediaTypeHeaderVersion1);
+                var response = await ExecuteGetRequestWithResponseAsync(baseUrl, headers: headers);
+                var myProfile = JsonConvert.DeserializeObject<UpdateStudentRestrictionResponseDto>(await response.Content.ReadAsStringAsync());
+
+                return myProfile;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Unable to GetF09StudentRestrictionAsync");
+                throw;
+            }
+        }
+
+        public async Task<HttpResponseMessage> PutF09StudentRestrictionAsync(UpdateStudentRestrictionRequestDto request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("userProfile", "User Profile cannot be null.");
+            }
+            try
+            {
+                var baseUrl = UrlUtility.CombineUrlPath(F09StudentRestriction);
+
+                var headers = new NameValueCollection();
+                headers.Add(AcceptHeaderKey, _mediaTypeHeaderVersion1);
+
+                var response = await ExecutePutRequestWithResponseAsync(request, baseUrl, headers: headers);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Unable to PutF09StudentRestrictionAsync.");
+                throw;
+            }
+        }
+    }
+}
---
+++ source/Ellucian.Colleague.Api/Ellucian.Colleague.Api.Client/ColleagueApiClient.F09.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ellucian.Colleague.Api.Client
+{
+    partial class ColleagueApiClient
+    {
+        private static readonly string F09ActiveRestrictions = "f09/active-restrictions";
+        private static readonly string F09StudentRestriction = "f09/student-restriction";
+        private static readonly string F09ActiveRestrictions2 = "f09/active-restrictions2";
+        private static readonly string F09StudentRestriction2 = "f09/student-restriction2";
+
+    }
+}
